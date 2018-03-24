4d3a07f6a07f1e2a8a28506d4973429e74ed3395
DanielPopOut <stephane.tchangang@gmail.com>
2018-03-24 10:32:02 -0400

    mise a jour guestcomponent --> meileur code hehehe

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -10,10 +10,7 @@ import { HomeCookCard } from '../home-cook-card';
 })
 export class EventComponent implements OnInit {
     public event: HomeCookEvent;
-    public userName: string;
-    public errorNameMessage: string;
     public cardTypeList = [new HomeCookCard("Question-Card", 0), new HomeCookCard("One-Vote-Card", 1), new HomeCookCard("Multiple-Vote-Card", 2)];
-    public cardTypeToCreate;
     public cardToCreate: HomeCookCard;
 
     constructor(private eventService: EventService) {
@@ -22,24 +19,6 @@ export class EventComponent implements OnInit {
 
     ngOnInit() {
         this.event = this.eventService.event;
-        console.log("username" + this.userName);
-
-    }
-
-    public chooseName(guest: string): void {
-        this.userName = guest;
-        console.log("username" + this.userName);
-    }
-
-    public addNewGuest(newGuestName: string): void {
-        console.log(newGuestName);
-        let validNewName: [boolean, string];
-        validNewName = this.eventService.addNewGuest(newGuestName);
-        if (!validNewName[0]) {
-            console.log(validNewName[1]);
-        } else {
-            this.userName = newGuestName;
-        }
     }
 
     public setTypeCardToCreate(homeCookCardToCreate: HomeCookCard) {
--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { HomeCookEvent } from '../home-cook-event';
 
 @Component({
-  selector: 'app-guest',
-  templateUrl: './guest.component.html',
-  styleUrls: ['./guest.component.css']
+    selector: 'app-guest',
+    templateUrl: './guest.component.html',
+    styleUrls: ['./guest.component.css']
 })
 export class GuestComponent implements OnInit {
 
-  constructor() { }
+    public event: HomeCookEvent;
+    public userName: string;
 
-  ngOnInit() {
-  }
+    constructor(private eventService: EventService) {
+    }
 
+    ngOnInit() {
+        this.event = this.eventService.event
+    }
+
+    public chooseName(guest: string): void {
+        this.userName = guest;
+        console.log("username" + this.userName);
+    }
+
+    public addNewGuest(newGuestName: string): void {
+        console.log(newGuestName);
+        let validNewName: [boolean, string];
+        validNewName = this.eventService.addNewGuest(newGuestName);
+        if (!validNewName[0]) {
+            console.log(validNewName[1]);
+        } else {
+            this.userName = newGuestName;
+        }
+    }
 }
